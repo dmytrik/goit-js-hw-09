68626c91275aed55727a691eba75559770db0ab1
dmytrik <107771156+dmytrik@users.noreply.github.com>
2022-07-14 18:39:42 +0000

    Deploying to gh-pages from @ dmytrik/goit-js-hw-09@d44f6d0fe5b54ce6129d28a55a112e217d5d8438 🚀

---
+++ 01-color-switcher.ac13a918.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),id:null};function e(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.start.addEventListener("click",(function(){t.id=setInterval(e,1e3),t.start.disabled=!0,t.stop.disabled=!1})),t.stop.addEventListener("click",(function(){t.stop.disabled=!0,t.start.disabled=!1,clearInterval(t.id)}));
+//# sourceMappingURL=01-color-switcher.ac13a918.js.map
