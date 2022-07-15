da682505fa5d8ba316a99c2a346368bcd0a06a47
Samoylenko Dmitry <107771156+dmytrik@users.noreply.github.com>
2022-07-15 09:55:09 +0300

    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -30,13 +30,31 @@ const options = {
   },
 };
 flatpickr(dateTime, options);
+
 function checkDate(currentDate, selectedDate) {
-  currentDate > selectedDate
-    ? Notiflix.Notify.failure('Please choose a date in the future')
-    : (start.disabled = false);
+  if (currentDate > selectedDate) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+    start.disabled = true;
+    return;
+  }
+  start.disabled = false;
+  // const timer = new Timer(selectedDate);
+  // start.addEventListener('click', timer.go.bind(timer));
 }
+
 start.addEventListener('click', startTimer);
+
 function startTimer() {
+  if (
+    days.textContent !== '00' ||
+    hours.textContent !== '00' ||
+    minutes.textContent !== '00' ||
+    seconds.textContent !== '00'
+  ) {
+    clearInterval(intervalId);
+    intervalId = setInterval(timerGo, 1000, selectedDate);
+    return;
+  }
   intervalId = setInterval(timerGo, 1000, selectedDate);
 }
 
@@ -47,22 +65,6 @@ function timerGo(selectedDate) {
   checkUI();
 }
 
-function updateUI(time) {
-  days.textContent = addLeadingZero(time.days);
-  hours.textContent = addLeadingZero(time.hours);
-  minutes.textContent = addLeadingZero(time.minutes);
-  seconds.textContent = addLeadingZero(time.seconds);
-}
-function checkUI() {
-  if (
-    days.textContent === '00' &&
-    hours.textContent === '00' &&
-    minutes.textContent === '00' &&
-    seconds.textContent === '00'
-  ) {
-    clearInterval(intervalId);
-  }
-}
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -81,6 +83,94 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+
+function updateUI(time) {
+  days.textContent = addLeadingZero(time.days);
+  hours.textContent = addLeadingZero(time.hours);
+  minutes.textContent = addLeadingZero(time.minutes);
+  seconds.textContent = addLeadingZero(time.seconds);
+}
+function checkUI() {
+  if (
+    days.textContent === '00' &&
+    hours.textContent === '00' &&
+    minutes.textContent === '00' &&
+    seconds.textContent === '00'
+  ) {
+    clearInterval(intervalId);
+    Notiflix.Notify.success('ЧАС ВИЙШОВ');
+  }
+}
+
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
+
+// class Timer {
+//   constructor(selectedDate) {
+//     this.selectedDate = selectedDate;
+//   }
+
+//   go() {
+//     if (
+//       days.textContent !== '00' ||
+//       hours.textContent !== '00' ||
+//       minutes.textContent !== '00' ||
+//       seconds.textContent !== '00'
+//     ) {
+//       clearInterval(intervalId);
+//       intervalId = setInterval(this.timerGo, 1000, this.selectedDate);
+//       return;
+//     }
+//     intervalId = setInterval(this.timerGo, 1000, this.selectedDate);
+//   }
+
+//   timerGo(selectedDate) {
+//     const currentDate = Date.now();
+//     const time = this.convertMs(selectedDate - currentDate);
+//     this.updateUI(time);
+//     this.checkUI();
+//   }
+
+//   updateUI(time) {
+//     days.textContent = this.addLeadingZero(time.days);
+//     hours.textContent = this.addLeadingZero(time.hours);
+//     minutes.textContent = this.addLeadingZero(time.minutes);
+//     seconds.textContent = this.addLeadingZero(time.seconds);
+//   }
+
+//   convertMs(ms) {
+//     // Number of milliseconds per unit of time
+//     const second = 1000;
+//     const minute = second * 60;
+//     const hour = minute * 60;
+//     const day = hour * 24;
+
+//     // Remaining days
+//     const days = Math.floor(ms / day);
+//     // Remaining hours
+//     const hours = Math.floor((ms % day) / hour);
+//     // Remaining minutes
+//     const minutes = Math.floor(((ms % day) % hour) / minute);
+//     // Remaining seconds
+//     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//     return { days, hours, minutes, seconds };
+//   }
+
+//   checkUI() {
+//     if (
+//       days.textContent === '00' &&
+//       hours.textContent === '00' &&
+//       minutes.textContent === '00' &&
+//       seconds.textContent === '00'
+//     ) {
+//       clearInterval(intervalId);
+//       Notiflix.Notify.success('ЧАС ВИЙШОВ');
+//     }
+//   }
+
+//   addLeadingZero(value) {
+//     return String(value).padStart(2, '0');
+//   }
+// }
